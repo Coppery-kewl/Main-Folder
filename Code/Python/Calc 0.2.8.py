74f85a9896719c850a25dc922e3897c317a70a9b
Coppery <coppery.original@gmail.com>
2026-02-20 01:05:10 +0300

    Add files via upload

---
+++ Code/Python/Calc 0.2.8.py
@@ -0,0 +1,9 @@
+#COPP CALC 0.2.8 By Coppery
+ver="0.2.8"
+print("Welcome to COPP CALC version ", ver, ", this supports 2 integers or floats and you can add them (for now), have fun! (or dont)")
+part1=input("Give first number: ")
+part2=input("Give second number: ")
+Ans= float(part1) + float(part2)
+print("The equation is: " + part1 + " + " + part2)
+print("The answer is: " + str(Ans))
+print("Hope you... appreciate it!!!!")
